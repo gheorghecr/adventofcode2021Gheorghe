7e907794f9944e32df0d9aa3da9bcab8c8e2cc84
Gheorghe Craciun <gheorghecr1997@gmail.com>
2021-12-02 14:56:03 +0000

    Added day 1 part 2

--- Day_1/solution.py
+++ Day_1/solution.py
@@ -5,10 +5,18 @@ def calculate_measurement_increases(input):
             measurement_increases += 1
     return measurement_increases
 
+def calculate_measurement_increases_part_2(input):
+    measurement_increases = 0
+    for index, value in enumerate(input[:-1]):
+        if sum(input[index: index + 3]) < sum(input[index + 1:  index + 4]):
+            measurement_increases += 1
+    return measurement_increases
+
 
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         input = [int(line.strip()) for line in f]
     print(calculate_measurement_increases(input))
+    print(calculate_measurement_increases_part_2(input))
     
--- Day_1/test_solution.py
+++ Day_1/test_solution.py
@@ -1,13 +1,21 @@
-from solution import calculate_measurement_increases
+from solution import calculate_measurement_increases, calculate_measurement_increases_part_2
 
-def test_1():
+def test_part_1_with_3_increases():
     input = [10, 20, 10, 40, 10, 200]
     assert 3 == calculate_measurement_increases(input)
 
-def test_2():
+def test_part_1_with_zero_increases():
     input = [10, 6]
     assert 0 == calculate_measurement_increases(input)
 
-def test_3():
+def test_part_1_with_one_increase():
     input = [10, 9, 8, 7, 6, 5, 11]
     assert 1 == calculate_measurement_increases(input)
+
+def test_part_2_with_2_increases():
+    input = [2, 9, 8, 15, 5, 6, 20]
+    assert 2 == calculate_measurement_increases_part_2(input)
+
+def test_part_2_with_0_increases():
+    input = [30, 9, 8, 15, 5, 6, 5]
+    assert 0 == calculate_measurement_increases_part_2(input)
